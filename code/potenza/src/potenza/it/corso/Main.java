fd42c519fb80fe91455b99634d9c9c8ecd2c9773
GrandeDito <grandedito9@gmail.com>
2022-11-24 17:25:03 +0100

    NumberDay

--- code/potenza/src/potenza/it/corso/Main.java
+++ code/potenza/src/potenza/it/corso/Main.java
@@ -1,24 +1,45 @@
 package potenza.it.corso;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		int numero = 2;
-		int potenza = 2;
 
-		for (int i = 0; i < 15; i++) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("inserisci 2 numeri");
+		int numero;
+		int elevazione;
+		int risultato;
+		//int potenza = 2;
+		
+		numero= input.nextInt();
+		elevazione= input.nextInt();
+		/*
+		for (int i = 0; i < elevazione; i++) {
 			potenza = numero * potenza;
 			System.out.println(potenza);
-		}
+		} 
 
-		int iw =0;
+		int iw = 0;
 		potenza = 2;
-		while (iw < 15) {
+		while (iw < elevazione) {
 			iw++;
 			potenza = numero * potenza;
 			System.out.println(potenza);
-		}
+		}  */
 
+		risultato = calcola(numero, elevazione);
+		System.out.println(risultato);
 		
+	} 
+	
+	public static int calcola(int numero, int elevazione){
+		int potenza = numero;
+		for (int i = 1; i < elevazione; i++) {
+			potenza = numero * potenza;
+			//System.out.println(potenza);
+		}
+		return potenza;		
 	}
 }
