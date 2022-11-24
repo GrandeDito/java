12fdbdfb4ad124f07942219826e8cb925fec0d56
GrandeDito <grandedito9@gmail.com>
2022-11-24 14:45:22 +0100

    somma-media

--- code/sumAverange/src/sumAverange/it/corso/Main.java
+++ code/sumAverange/src/sumAverange/it/corso/Main.java
@@ -9,12 +9,14 @@ public class Main {
 		Scanner input = new Scanner(System.in);
 		System.out.println("inserisci 2 numeri");
 		int n1, n2;
+		
 		n1= input.nextInt();
 		n2= input.nextInt();
+		double media =(double) (n1 + n2) / 2;
 		System.out.println("somma : ");
 		System.out.println( n1 + n2);
-		System.out.println("media : ");
-		System.out.println((n1 + n2) / 2);
+		System.out.println("media : " + media);
+		
 		
 	}
 
