94a9af49879ca0918a805d97276272df4c629042
GrandeDito <grandedito9@gmail.com>
2022-11-24 12:38:31 +0100

    java program

--- code/Accademy/src/accademy/it/corso/Main.java
+++ code/Accademy/src/accademy/it/corso/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("ciao");
+		System.out.println("hello \n");
+		System.out.println("Adriano silletti");
 	}
 
 }
