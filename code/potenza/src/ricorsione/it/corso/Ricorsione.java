e9fb42f76968cdd5277e5929d1285c68ab093fb5
GrandeDito <grandedito9@gmail.com>
2022-11-24 17:49:22 +0100

    potenza+fattoriale

---
+++ code/potenza/src/ricorsione/it/corso/Ricorsione.java
@@ -0,0 +1,17 @@
+package ricorsione.it.corso;
+
+public class Ricorsione {
+	public static void main(String[] args) {
+		int a =130;
+		int risultato = f(a);
+		System.out.println(risultato);
+	}
+
+	public static int f(int n) {
+		if (n % 2 == 1) {
+			return 0;
+		} else {
+			return 1 + f(n / 2);
+		}
+	}
+}
