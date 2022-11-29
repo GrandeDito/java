e3818e3acbf57efcad4b75067966bfc49263c79e
GrandeDito <grandedito9@gmail.com>
2022-11-29 09:54:01 +0100

    Bubble-sort

---
+++ code/BubbleSort/src/com/bubblesort/Main.java
@@ -0,0 +1,42 @@
+package com.bubblesort;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int n;
+
+		int arr[] = new int[10];
+		System.out.println("inserisci 10 numeri: ");
+		for (int i = 0; i < 10; i++) {
+			n = input.nextInt();
+			arr[i] = n;
+
+		}
+
+		bubbleSort(arr);
+		System.out.println("Bubble Sort");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+
+	static void bubbleSort(int[] arr) {
+		int n = arr.length;
+		int temp = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < (n - i); j++) {
+				if (arr[j - 1] > arr[j]) {
+					temp = arr[j - 1];
+					arr[j - 1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	}
+
+}
